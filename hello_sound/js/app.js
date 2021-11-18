81da7e0a9a1315dfb02b912c16c23fb33bb37f4d
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-18 21:35:09 +0000

    now 4 waves!

--- hello_sound/js/app.js
+++ hello_sound/js/app.js
@@ -1,8 +1,16 @@
 "use strict";
 
+// 4 variables that represent oscillator waveforms
 var audioContext = new AudioContext();
+var saw = "sawtooth";
+var sine = "sine";
+var tri = "triangle";
+var square = "square";
+// a variable intended to contain one of these waveforms
+var currentWaveform = undefined
+currentWaveform = square;
+// start of oscillator
 var osc = audioContext.createOscillator();
-var waveformType = "sawtooth"; // oscillator variable
-osc.type = waveformType;
+osc.type = currentWaveform; // Assigned it to our oscilator type
 osc.connect(audioContext.destination);
 osc.start(audioContext.currentTime);
