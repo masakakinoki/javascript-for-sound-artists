7b43c1fa0cfc3c0aadb6ee24ac3757dc7e4f3615
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-20 12:08:50 +0000

    added frequency update function

--- html_css/js/app.js
+++ html_css/js/app.js
@@ -7,11 +7,32 @@ window.onload = function () {
   /*BEGIN set initial osc state to false*/
   let osc = false;
   /* END set initial osc state to false */
+
+  let freqSliderVal = document.getElementsByTagName("input")[1].value;
+
+  /*   BEGIN check range slider value and set frequency of oscillator*/
+  setInterval(function () {
+
+    if (!osc) {
+
+      console.log("Oscillator is stopped. Waiting for oscillator to start");
+
+    } else {
+
+      freqSliderVal = document.getElementsByTagName("input")[1].value;
+      osc.frequency.value = freqSliderVal;
+      console.log("Oscillator is playing. Frequency value is " + freqSliderVal);
+    }
+
+  }, 50);
+  /* End check range slider value and set frequency of oscillator*/
+
   onOff.addEventListener("click", function () {
+
     if (!osc) {
       osc = audioContext.createOscillator();
       osc.type = "sawtooth";
-      osc.frequency.value = 300;
+      osc.frequency.value = freqSliderVal;
       osc.connect(audioContext.destination);
       osc.start(audioContext.currentTime);
       onOff.value = "stop";
