55afce527ac8285b339ebdb77a5b5b5cb02d4060
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-20 17:21:50 +0000

    chapter 13 done

---
+++ chapter_13/js/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var sound = audioBatchLoader({
+
+    kick: "sounds/kick.mp3",
+    snare: "sounds/snare.mp3",
+    hihat: "sounds/hihat.mp3",
+    shaker: "sounds/shaker.mp3"
+
+});
+
+
+window.addEventListener("mousedown", function() {
+
+    sound.kick.play();
+    sound.snare.play();
+
+});
