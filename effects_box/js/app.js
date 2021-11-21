06921cfdce81325aa7db884303042762f0c9d36e
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-21 17:23:33 +0000

    added start/stop button and beature to effects_box

--- effects_box/js/app.js
+++ effects_box/js/app.js
@@ -1,11 +1,17 @@
 "use strict";
 let AudioContext = window.AudioContext || window.webkitAudioContext;
-let audioContext = new AudioContext();
+let audioCtx = new AudioContext();
+let osc_1;
+let osc_2;
+let filter;
+let oscState = false;
+console.log(oscState);
+
 // begin custom sound
 function customSound(filterVal) {
-  var osc_1 = audioContext.createOscillator();
-  var osc_2 = audioContext.createOscillator();
-  var filter = audioContext.createBiquadFilter();
+  osc_1 = audioCtx.createOscillator();
+  osc_2 = audioCtx.createOscillator();
+  filter = audioCtx.createBiquadFilter();
   filter.type = "lowpass";
   osc_1.type = "sawtooth";
   osc_1.frequency.value = 30;
@@ -14,23 +20,25 @@ function customSound(filterVal) {
   filter.frequency.value = filterVal || filter.frequency.valu;
   osc_1.connect(filter);
   osc_2.connect(filter);
-  filter.connect(audioContext.destination);
-  osc_1.start(audioContext.currentTime);
-  osc_2.start(audioContext.currentTime);
+  filter.connect(audioCtx.destination);
+  osc_1.start(audioCtx.currentTime);
+  osc_2.start(audioCtx.currentTime);
+  oscState = true;
+  console.log(oscState);
 }
 
 // square wave
-function square(filterVal) {
-  var osc = audioContext.createOscillator();
-  var filter = audioContext.createBiquadFilter();
-  filter.type = "lowpass";
-  osc.type = "square";
-  osc.frequency.value = 400;
-  filter.frequency.value = filterVal || filter.getFrequencyResponse.value;
-  osc.connect(filter);
-  filter.connect(audioContext.destination);
-  osc.start(audioContext.currentTime);
-}
+// function square(filterVal) {
+//   let osc = audioCtx.createOscillator();
+//   let filter = audioCtx.createBiquadFilter();
+//   filter.type = "lowpass";
+//   osc.type = "square";
+//   osc.frequency.value = 400;
+//   filter.frequency.value = filterVal || filter.getFrequencyResponse.value;
+//   osc.connect(filter);
+//   filter.connect(audioCtx.destination);
+//   osc.start(audioCtx.currentTime);
+// }
 
 function effectsBox(sourceInput, filterParam) {
   sourceInput(filterParam);
@@ -38,8 +46,25 @@ function effectsBox(sourceInput, filterParam) {
 
 effectsBox(customSound, 130);
 
-function touchStarted() {
-  if (getAudioContext().state !== "running") {
-    getAudioContext().resume();
+document.getElementById("play-button").addEventListener("click", function () {
+  
+  if (oscState == false) {
+    console.log("it's not running well!");
+    console.log(audioCtx.state);
+    osc_1.connect(filter);
+    osc_2.connect(filter);
+    oscState = true;
+  } else {
+    console.log("it's running well!");
+    console.log(audioCtx.state);
+    osc_1.disconnect();
+    osc_2.disconnect();
+    oscState = false;
   }
-}
+});
+
+// function touchStarted() {
+//   if (audioCtx().state !== "running") {
+//     audioCtx().resume();
+//   }
+// }
