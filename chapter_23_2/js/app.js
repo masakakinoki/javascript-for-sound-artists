b360801667f16db1a75c776142c940dc4b3976ae
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-22 15:15:26 +0000

    added metronome

--- chapter_23_2/js/app.js
+++ chapter_23_2/js/app.js
@@ -2,43 +2,58 @@
 
 let futureTickTime = audioCtx.currentTime;
 let counter = 1;
+let tempo = 80;
+let secondsPerBeat = 60 / tempo;
+let counterTimeValue = (secondsPerBeat / 4); // 16th note
 let osc = audioCtx.createOscillator();
 let metronomeVolume = audioCtx.createGain();
+
 function playMetronome(time) {
     osc = audioCtx.createOscillator();
     osc.connect(metronomeVolume);
     metronomeVolume.connect(audioCtx.destination);
+    osc.frequency.value = 500;
     osc.start(time);
     osc.stop(time + 0.1);
 }
 
 function scheduler() {
-    // console.log("scheduler now!");
+    console.log("scheduler now!");
 
     if (futureTickTime < audioCtx.currentTime + 0.1) {
-        // console.log("This is beat: " + counter);
+        console.log("This 16th note is: " + counter);
+        console.log("16th is: " + counterTimeValue);
+        // console.log("time: " + time);
+        playMetronome(futureTickTime);
         console.log("futureTickTime: " + futureTickTime);
         console.log("Web Audio Time: " + audioCtx.currentTime);
-        futureTickTime += 0.5; //can be any time value. 0.5 happens to be a quarter note at 120bpm
-        
+        futureTickTime += counterTimeValue; //can be any time value. 0.5 happens to be a quarter note at 120bpm
+
+        if (counter === 1) {
+            osc.frequency.value = 500;
+        } else {
+            osc.frequency.value = 300;
+        }
+
         counter += 1;
-        if (counter > 4) {
+        if (counter > 16) {
             counter = 1;
         }
     }
     window.setTimeout(scheduler, 0);
 }
+
 scheduler();
 
 
-var kick = audioFileLoader("sounds/kick.mp3");
-var snare = audioFileLoader("sounds/snare.mp3");
-var hihat = audioFileLoader("sounds/hihat.mp3");
+// var kick = audioFileLoader("sounds/kick.mp3");
+// var snare = audioFileLoader("sounds/snare.mp3");
+// var hihat = audioFileLoader("sounds/hihat.mp3");
 
 
 
-var tempo = 120; //_____BPM (beats per minute)
-var eighthNoteTime = (60 / tempo) / 2;
+// var tempo = 120; //_____BPM (beats per minute)
+// var eighthNoteTime = (60 / tempo) / 2;
 
 function playDrums() {
     // Play 4 bars of the following:
@@ -59,14 +74,14 @@ function playDrums() {
     }
 }
 
-window.addEventListener("mousedown", function () {
-    playDrums();
-    // console.log(sound);
-    // sound.kick.play();
-    // sound.snare.play(0.1, 0.1, 3.0);
-    // sound.hihat.play(0.3);
-    // sound.shaker.play(0.4)
-});
+// window.addEventListener("mousedown", function () {
+// playDrums();
+// console.log(sound);
+// sound.kick.play();
+// sound.snare.play(0.1, 0.1, 3.0);
+// sound.hihat.play(0.3);
+// sound.shaker.play(0.4)
+// });
 
 // window.addEventListener("mouseup", function () {
 // console.log(sound);
