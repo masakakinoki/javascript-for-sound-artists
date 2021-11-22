5f3b3cb38169d10a1daf9f9fbe64a9136d6d6985
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-22 13:23:37 +0000

    added chapter_23 and managed to make it play the drum pattern

---
+++ chapter_23/js/app.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var kick = audioFileLoader("sounds/kick.mp3");
+var snare = audioFileLoader("sounds/snare.mp3");
+var hihat = audioFileLoader("sounds/hihat.mp3");
+
+
+
+var tempo = 120; //_____BPM (beats per minute)
+var eighthNoteTime = (60 / tempo) / 2;
+
+function playDrums() {
+    // Play 4 bars of the following:
+    for (var bar = 0; bar < 4; bar++) {
+        var time = bar * 8 * eighthNoteTime;
+        // Play the bass (kick) drum on beats 1, 5
+        kick.play(time);
+        kick.play(time + 4 * eighthNoteTime);
+
+
+        // Play the snare drum on beats 3, 7
+        snare.play(time + 2 * eighthNoteTime);
+        snare.play(time + 6 * eighthNoteTime);
+        // Play the hi-hat every eighth note.
+        for (var i = 0; i < 8; ++i) {
+            hihat.play(time + i * eighthNoteTime);
+        }
+    }
+}
+
+window.addEventListener("mousedown", function () {
+    playDrums();
+    // console.log(sound);
+    // sound.kick.play();
+    // sound.snare.play(0.1, 0.1, 3.0);
+    // sound.hihat.play(0.3);
+    // sound.shaker.play(0.4)
+});
+
+// window.addEventListener("mouseup", function () {
+    // console.log(sound);
+    // sound.kick.stop();
+    // sound.snare.stop();
+    // sound.hihat.play(0.3);
+    // sound.shaker.play(0.4)
+// }); 
+
+
+/* window.setTimeout(function() {
+    playDrums();
+}, 1500); */
+
+/* 
+console.log(audioCtx.currentTime);
+
+const sound = audioBatchLoader({
+
+    kick: "sounds/kick.mp3",
+    snare: "sounds/snare.mp3",
+    hihat: "sounds/hihat.mp3",
+    shaker: "sounds/shaker.mp3",
+    nodes: function (sound) {
+        let gain = audioCtx.createGain();
+        sound.connect(gain);
+        gain.gain.value = 0.5;
+        gain.connect(audioCtx.destination);
+    }
+});
+*/
