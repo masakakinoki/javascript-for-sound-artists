f86b9da4012ddeb236dd6644f26ccf2d25790fdb
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-22 12:31:49 +0000

    chapter_21 done

--- chapter_21/js/app.js
+++ chapter_21/js/app.js
@@ -1,28 +1,59 @@
 "use strict";
-// for cross browser
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+$(function () {
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioCtx = new AudioContext();
+  let analyzer = audioCtx.createAnalyser();
+  let osc = audioCtx.createOscillator();
 
-console.log(audioCtx.state);
+  //Create typed array
+  let frequencyData = new Uint8Array(analyzer.frequencyBinCount);
+  //Store frequency data
+  analyzer.getByteFrequencyData(frequencyData);
 
-document.getElementById("play-button").addEventListener("click", function () {
-  
-  if (audioCtx.state !== "running") {
-    console.log("it's not running well");
-    // osc_1.connect(filter);
-    // osc_2.connect(filter);
-    // oscState = true;
-    audioCtx.resume();
-    // audioCtx.onstatechange = () => console.log(audioCtx.state);
-  } else {
-    console.log("it's running");
-    // osc_1.disconnect();
-    // osc_2.disconnect();
-    // oscState = false;
-    audioCtx.suspend();
-    // console.log(audioCtx.state);
-    
+  console.log("frequencyData.length: " + frequencyData.length); //1024
+  console.log(frequencyData);
+
+  let app = $(".app");
+  let bars = undefined;
+
+  osc.frequency.value = 5;
+  osc.type = "sawtooth";
+  osc.connect(analyzer);
+  analyzer.connect(audioCtx.destination);
+  osc.start(audioCtx.currentTime);
+  analyzer.fftSize = 64;
+  console.log("analyzer.frequencyBinCount: " + analyzer.frequencyBinCount); //1024
+
+  //  BEGIN visualization
+
+  $(".bin-count-number").text(analyzer.fftSize / 2); //Bin count
+
+  for (let i = 0; i < analyzer.frequencyBinCount; i++) {
+
+    $(".app").append("<div></div><span>" + i + "</span>");
+
   }
-});
 
-audioCtx.onstatechange = () => console.log(audioCtx.state);
+  bars = $(".app > div");
+
+  function update() {
+
+    requestAnimationFrame(update);
+
+    analyzer.getByteFrequencyData(frequencyData);
+
+    for (let i = 0; i < bars.length; i += 1) {
+
+      bars[i].style.height = frequencyData[i] + 'px';
+
+    }
+
+  }
+
+  update();
+
+  //END visualization
+
+
+
+});
